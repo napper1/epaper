5a44a73ed39d2dda3044ce9e26c9f61be4ee4538
Ben <napper197@gmail.com>
2021-09-04 17:47:24 +1000

    Fix fonts import

--- client.py
+++ client.py
@@ -30,11 +30,9 @@ class EpaperClient(object):
     fonts_dir = os.path.join(project_dir, "fonts")
     local_file_dir = os.path.join(project_dir, "media")
     small_font = ImageFont.truetype(os.path.join(fonts_dir, "OpenSans-Regular.ttf"), 16)
-    font_awesome = ImageFont.truetype(
-        os.path.join(fonts_dir, "fonts/fa-regular-400.ttf"), 30
-    )
+    font_awesome = ImageFont.truetype(os.path.join(fonts_dir, "fa-regular-400.ttf"), 30)
     font_awesome_solid = ImageFont.truetype(
-        os.path.join(fonts_dir, "fonts/fa-solid-900.ttf"), 30
+        os.path.join(fonts_dir, "fa-solid-900.ttf"), 30
     )
     # font awesome icons
     # Most use FA Solid instead of regular. See FA docs
